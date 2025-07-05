02692da26888cae0c2a86db931de844a479ef004
mlm-studios <you@example.com>
2025-07-05 09:41:31 +0530

    add second level

--- game/scenes/w1_level1.gd
+++ game/scenes/w1_level1.gd
@@ -1,16 +1,21 @@
 extends Control
 
-const HINT_PREFIX = "hint_"
-var level_name = self.get_script().get_path().split("/")[-1].trim_suffix(".gd")
+const HINT_PREFIX = "Hint"
 
 var edited_once := false
+@export var edited_once_tip : String
 var hint_clicked := false
 var edited_once_after_hint := false
+@export var edited_once_after_hint_tip : String
+@export var output_string : String
 
 enum State {DIALOGUE, EDITING, COMPLETED}
 
 @export var hint_dialogue : DialogueResource
 @export var level_dialogue : DialogueResource
+@export var next_scene : PackedScene
+
+@onready var level_name = Engine.get_main_loop().current_scene.name
 
 @onready var code_area: CodeEdit = %CodeArea
 @onready var ans_area: CodeEdit = %AnsArea
@@ -22,27 +27,30 @@ enum State {DIALOGUE, EDITING, COMPLETED}
 func  _ready() -> void:
 	code_area.text_changed.connect(_on_code_changed)
 	hint_button.pressed.connect(func():
-		DialogueManager.show_dialogue_balloon(hint_dialogue, HINT_PREFIX + level_name)
+		DialogueManager.show_dialogue_balloon(hint_dialogue, level_name + HINT_PREFIX)
 		hint_clicked = true
 		)
 	
-	DialogueManager.show_dialogue_balloon(level_dialogue, "start")
+	DialogueManager.show_dialogue_balloon(level_dialogue, level_name + "Start")
 	
 	UIEffects.typewriter_effect(%TipLabel)
 
 func _on_code_changed():
 	if not edited_once:
 		edited_once = true
-		%TipLabel.text = "[wave]Ignore the [i]class void main[/i] part for now, thats just the boilerplate."
+		%TipLabel.text = edited_once_tip
 		UIEffects.typewriter_effect(%TipLabel)
 	
 	if hint_clicked and not edited_once_after_hint:
-		%TipLabel.text = "[wave]The bug appears to be present in [i][b]line 3[/b][/i]."
+		%TipLabel.text = edited_once_after_hint_tip
 		UIEffects.typewriter_effect(%TipLabel)
 	
 	
 	if code_area.text == ans_area.text:
+		if %OutputArea.visible: %OutputArea.text = output_string
 		print("Level completed")
-		DialogueManager.show_dialogue_balloon(level_dialogue, level_name + "_end")
+		DialogueManager.show_dialogue_balloon(level_dialogue, level_name + "End")
+		await DialogueManager.dialogue_ended
+		Transitions.change_scene_with_transition_packed(next_scene)
 	
 	
