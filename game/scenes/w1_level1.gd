7089315f20305fcfe8f18e86b623fdcb35855c07
mlm-studios <you@example.com>
2025-07-05 07:38:11 +0530

    add a tip label, and some conditional statements

--- game/scenes/w1_level1.gd
+++ game/scenes/w1_level1.gd
@@ -1,9 +1,16 @@
 extends Control
 
 const HINT_PREFIX = "hint_"
-var hint_name = self.get_script().get_path().split("/")[-1].trim_suffix(".gd")
+var level_name = self.get_script().get_path().split("/")[-1].trim_suffix(".gd")
+
+var edited_once := false
+var hint_clicked := false
+var edited_once_after_hint := false
 
 enum State {DIALOGUE, EDITING, COMPLETED}
+
+@export var hint_dialogue : DialogueResource
+@export var level_dialogue : DialogueResource
 
 @onready var code_area: CodeEdit = %CodeArea
 @onready var ans_area: CodeEdit = %AnsArea
@@ -14,12 +21,28 @@ enum State {DIALOGUE, EDITING, COMPLETED}
 
 func  _ready() -> void:
 	code_area.text_changed.connect(_on_code_changed)
-	hint_button.pressed.connect(DialogueManager.show_dialogue_balloon.bind(preload("uid://br0g15mlkqp0p"), HINT_PREFIX + hint_name))
+	hint_button.pressed.connect(func():
+		DialogueManager.show_dialogue_balloon(hint_dialogue, HINT_PREFIX + level_name)
+		hint_clicked = true
+		)
 	
-	DialogueManager.show_dialogue_balloon(preload("uid://bcyitiwfyenvi"), "start")
+	DialogueManager.show_dialogue_balloon(level_dialogue, "start")
 	
-	
+	UIEffects.typewriter_effect(%TipLabel)
 
 func _on_code_changed():
+	if not edited_once:
+		edited_once = true
+		%TipLabel.text = "[wave]Ignore the [i]class void main[/i] part for now, thats just the boilerplate."
+		UIEffects.typewriter_effect(%TipLabel)
+	
+	if hint_clicked and not edited_once_after_hint:
+		%TipLabel.text = "[wave]The bug appears to be present in [i][b]line 3[/b][/i]."
+		UIEffects.typewriter_effect(%TipLabel)
+	
+	
 	if code_area.text == ans_area.text:
 		print("Level completed")
+		DialogueManager.show_dialogue_balloon(level_dialogue, level_name + "_end")
+	
+	
